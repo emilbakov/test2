e91c1ef6db9afb657597c0f4f289b130d732b37b
m o <bakovemil@gmail.com>
2022-09-01 22:29:13 +0200

    2

--- huntmap/urls.py
+++ huntmap/urls.py
@@ -1,5 +1,7 @@
 from django.conf import settings
-from django.conf.urls import url
+# from django.conf.urls import url
+from django.urls import re_path
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -9,7 +11,7 @@ from core.models import HuntSpot
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=HuntSpot, properties=('title', 'description', 'picture_url')), name='data')
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    re_path(r'^data.geojson$', GeoJSONLayerView.as_view(model=HuntSpot, properties=('title', 'description', 'picture_url')), name='data')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
